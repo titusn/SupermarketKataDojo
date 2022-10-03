5f54abb4af648b4806f026f0fd0cf806e38fa26c
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2022-10-03 16:45:55 +0200

    Reset kata

--- src/main/java/com/titusnachbauer/supermarket/ReceiptPrinter.java
+++
@@ -1,9 +0,0 @@
-package com.titusnachbauer.supermarket;
-
-public class ReceiptPrinter {
-    public String printItem(Item item, int quantity) {
-        // For the name of an item we need 26 columns and it will
-        // be displayed from left hand side
-        return String.format("%-26s%s%6s", item.getName(), "EUR", item.getAmount().toString());
-    }
-}
